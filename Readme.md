26e91d8e09ff8e93899a287bd5ab9b4708372420
Username-ajansons <85576462+Username-ajansons@users.noreply.github.com>
2022-01-16 14:16:23 +0000

    FirstCommit

---
+++ Readme.md
@@ -0,0 +1,31 @@
+# Mixed Messages
+
+This project is a part of Codecademy Full-Stack Engineer course. The task was to create a random message generator that takes in three arrays and randomly combines statements for the final output. The content of the output was inspired by popular game *__“Cards Against Humanity”__* with some added features. The result is logged into the console.
+
+## Contents
+
+* [General Information](#general-information)
+* [Technologies](#technologies)
+* [Executable enviroment](#executable-enviroment)
+* [Project Status](#project-status)
+
+## General Information
+
+This project contains three arrays that are passed to math functions to randomly select a single item from each array. The single function, within this project, is responsible for taking the random messages and combining them into one sentence. The final output is presented in the console.
+
+## Technologies
+
+* Visual Studio Code
+* Javscript
+* Markdown
+* Github
+* Git Bash
+* Node.js
+
+## Executable enviroment
+
+The script can executed by using node.js.
+
+## Project Status
+
+Completed.# Mixed-MessagesV1
---
+++ script.js
@@ -0,0 +1,22 @@
+const firstArr = ['Breaking news:', 'The Sun reports:', 'The Guardian headline:', 'Today in the news:', 'My teacher told me that:'];
+
+const secondArr = ['The secret to a lasting marriage is comunication, comunication and', 'Only two things in life are certain: death and', 'The healing process began when I joined a support group for victoms of',
+'A succesful job interview starts with a firm handshake and ends with', 'It is a pity that kids these days are all involved with', 'In Rome, there are whispers that the Vatican has a secret Room devoted to',
+'Next from J.K. Rowling: Harry Potter and the Chamber of'];
+
+const thirdArr = ['spending lots of money.', 'a really cool hat.', 'drinking alone.', 'my ex-wife.', 'Stalin.', 'Vladimir Putin.', 'Santa Claus.', 'the American Dream.', 'the economy.', 'not wearing pants.',
+ 'Italians.', 'soup that is too hot.', 'the World of Warcraft.', 'Jesus.', 'shaking the baby until it stops crying.', 'puppies!', 'seeing my father cry.', 'being a woman.', 'the Russians.',
+'destroying the evidence.', 'Lady Gaga.', 'extremely tight jeans.', 'the true meaning of Christmas.', 'shiny objects.', 'Robert Downey Jr.'];
+
+function randomMessage() {
+    const firstRandSel = firstArr[Math.floor(Math.random() * firstArr.length)]
+    const secondRandSel = secondArr[Math.floor(Math.random() * secondArr.length)]
+    const thirdRandSel = thirdArr[Math.floor(Math.random() * thirdArr.length)]
+
+    const fStatement = (arrOne, arrTwo, arrThree) => arrOne + ' ' + arrTwo + ' ' + arrThree;
+
+    const output = fStatement(firstRandSel, secondRandSel, thirdRandSel);
+    const htmlTag = document.getElementById('random-message');
+
+    htmlTag.innerHTML = output;
+};
